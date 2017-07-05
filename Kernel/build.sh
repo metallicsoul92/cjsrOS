5ca676088c7b62f5d21f69b28e09d1bab318d933
climjark <climjark@protonmail.com>
2017-07-05 00:24:01 -0400

    Added the option to build as a kernel library as well as user space library, BE CAREFUL AT THE MOMENT I PULL A CROSSCOMPILED GCC OBJECT FILE IN: Kernel/build.sh look for crtbegin.o and crtend.o
    need to implement own, so errors dont occur per platform
    
    
    Signed-off-by: climjark <climjark@protonmail.com>

--- Kernel/build.sh
+++ Kernel/build.sh
@@ -7,12 +7,12 @@ nasm -f elf32 crti.asm -o bin/crti.o
 echo -e "Compiling crtn.asm"
 nasm -f elf32 crtn.asm -o bin/crtn.o
 echo -e "adding crtbegin.o"
-gcc $CFLAGS -print-file-name=crtbegin.o
+cp $(i686-elf-gcc $CFLAGS -print-file-name=crtbegin.o) bin/crtbegin.o
 echo -e "adding crtend.o"
-gcc $CFLAGS -print-file-name=crtend.o
+cp $(i686-elf-gcc $CFLAGS -print-file-name=crtend.o) bin/crtend.o
 echo -e "compiling kernel files:"
 echo -e "kernel.c"
-gcc -m32 src/kernel.c -o bin/main.o -nostdlib -nostdinc -fno-builtin -fno-stack-protector
+gcc -m32 src/kernel.c -o bin/main.o  -ffreestanding -nostdlib -nostdinc -fno-builtin -fno-stack-protector
 echo -e "-linking kernelfiles"
 set -x
-ld -T linker.ld -melf_i386 bin/boot.o bin/main.o  -o bin/kernel32.bin
+ld -T linker.ld -melf_i386  bin/crti.o bin/crtbegin.o bin/boot.o bin/main.o  bin/crtend.o bin/crtn.o  -o bin/kernel32.bin
